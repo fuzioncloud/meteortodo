6b1e40c35abdf055c28af59ad9271cf87c69cf13
Zebadiah Zaharris <fuzioncloud@icloud.com>
2015-03-11 09:21:34 -0700

    CH 8 Storing temp UI state in Session

--- meteorTodo.js
+++ meteorTodo.js
@@ -5,20 +5,36 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: function () {
       // To create tasks:
       // from a terminal in the app directory, run
       //    `meteor mongo'
       // to console into the app's local development database. At the prompt create tasks with:
       //    `db.tasks.insert({ text: "text for task here", createdAt: new Date() });`
-      return Tasks.find({}, {
-        // Show newest tasks first
-        sort: {
-          createdAt: -1
-        }
-      });
+    tasks: function () {
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {
+          sort: {
+            createdAt: -1
+          }
+        });
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {
+          sort: {
+            createdAt: -1
+          }
+        });
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
+
 
   // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
   Template.body.events({
@@ -37,16 +53,19 @@ if (Meteor.isClient) {
 
       // Prevent default form submit
       return false;
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
   // below everything else within the client code
   Template.task.events({
-    "click .toggle-checked": function() {
+    "click .toggle-checked": function () {
       // Set the checked property to the opposite of its current value
       Tasks.update(this._id, {$set: {checked: ! this.checked}});
     },
-    "click .delete": function() {
+    "click .delete": function () {
       Tasks.remove(this._id);
     }
   });
