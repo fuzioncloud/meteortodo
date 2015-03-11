bd84beb6830764349dfa7b2e7adf42b5978c9522
Zebadiah Zaharris <fuzioncloud@icloud.com>
2015-03-11 05:09:09 -0700

    Step 4 completed. Add, Insert, sort tasks; Attach events to templates

--- meteorTodo.js
+++ meteorTodo.js
@@ -5,8 +5,38 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: function() {
-      return Tasks.find({});
+    tasks: function () {
+      // To create tasks:
+      // from a terminal in the app directory, run
+      //    `meteor mongo'
+      // to console into the app's local development database. At the prompt create tasks with:
+      //    `db.tasks.insert({ text: "text for task here", createdAt: new Date() });`
+      return Tasks.find({}, {
+        // Show newest tasks first
+        sort: {
+          createdAt: -1
+        }
+      });
+    }
+  });
+
+  // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
+  Template.body.events({
+    "submit .new-task": function(event) {
+      // This function is called when the new task form is submitted
+
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
     }
   });
 
